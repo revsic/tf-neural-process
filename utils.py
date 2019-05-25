1f2444fef185db6583601736e6beacc3b844489d
revsic <revsic99@gmail.com>
2019-05-25 14:51:21 +0900

    utils: Impl method plot_func

--- utils.py
+++ utils.py
@@ -22,4 +22,16 @@ def batch_mlp(input_tensor, model):
     return output
 
 
-# def plot_func(x, y, cx, cy, )
+def plot_func(x, y, cx, cy, pred, var, batch=0, axis=0):
+    plt.plot(x[batch], pred[batch], 'b', linewidth=2)
+    plt.plot(x[batch], y[batch], 'k:', linewidth=2)
+    plt.plot(cx[batch], cy[batch], 'ko', markersize=10)
+    plt.fill_between(
+        x[batch, :, axis], 
+        pred[batch, :, axis] - var[batch, :, axis],
+        pred[batch, :, axis] + var[batch, :, axis],
+        alpha=0.2,
+        facecolor='#65c9f7',
+        interpolate=True)
+
+    plt.show()
