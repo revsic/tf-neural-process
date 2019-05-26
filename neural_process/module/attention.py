cfe9cb63195daf24904871e6773632bfb108bfdb
revsic <revsic99@gmail.com>
2019-05-27 03:03:38 +0900

    attn: Move to np.module

---
+++ neural_process/module/attention.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+
+from neural_process.module.tfutils import dense_sequential
+
+def uniform_attention(query, _, value):
+    # query : [B, T, X]
+    # value : [B, C, X]
+    n_target = tf.shape(query)[1]
+    rep = tf.reduce_mean(value, axis=1, keepdims=True)
+    rep = tf.tile(rep, [1, n_target, 1])
+    # [B, T, X]
+    return rep
+
+
+def laplace_attention(query, key, value):
+    # query : [B, T, X]
+    # key : [B, C, X]
+    # value : [B, C, X]
+    key = tf.expand_dims(key, axis=1)
+    query = tf.expand_dims(query, axis=2)
+    # unnorm_weights : [B, T, C, X]
+    unnorm_weights = -tf.abs((key - query))
+    # unnorm_weights : [B, T, C]
+    unnorm_weights = tf.reduce_sum(unnorm_weights, axis=-1)
+    weights = tf.nn.softmax(unnorm_weights)
+    # [B, T, X]
+    return weights @ value
+
+
+def dotprod_attention(query, key, value):
+    # query : [B, T, X]
+    # key : [B, C, X]
+    # value : [B, C, X]
+    d_k = tf.shape(key)[-1]
+    scale = tf.sqrt(tf.cast(d_k, tf.float32))
+    # unnorm_weights : [B, T, C]
+    unnorm_weights = query @ tf.transpose(key, [0, 2, 1])
+    weights = tf.nn.softmax(unnorm_weights / scale)
+    # [B, T, X]
+    return weights @ value
+
+
+class MultiheadAttention:
+    def __init__(self, n_head):
+        self.n_head = n_head
+        self.params = None
+    
+    def __call__(self, query, key, value):
+        if self.params is None:
+            d_v = value.shape[-1].value
+            self.params = self.get_params(d_v)
+
+        rep = tf.constant(0.)
+        for param in self.params:
+            o = dotprod_attention(param['dq'](query),
+                                   param['dk'](key),
+                                   param['dv'](value))
+            rep += param['do'](o)
+
+        return rep
+
+    def get_params(self, d_v):
+        params = []
+        head_size = d_v / self.n_head
+        for _ in range(self.n_head):
+            param = {}
+            param['dq'] = tf.keras.layers.Dense(head_size)
+            param['dk'] = tf.keras.layers.Dense(head_size)
+            param['dv'] = tf.keras.layers.Dense(head_size)
+            param['do'] = tf.keras.layers.Dense(d_v)
+            params.append(param)
+        return params
+
+
+class Attention:
+    def __init__(self, rep, attention_type, output_sizes=None, n_head=8):
+        self.rep = rep
+        self.attention_type = attention_type
+
+        if self.rep not in ['identity', 'mlp']:
+            raise ValueError("'rep' should be one of ['identity', 'mlp']")
+        if self.attention_type not in ['uniform', 'laplace', 'dotprod', 'multihead']:
+            raise ValueError("'attention_type should be one of ['uniform', 'laplace', 'dotprod', 'multihead']")
+
+        if self.rep == 'mlp':
+            self.dk = dense_sequential(output_sizes)
+            self.dq = dense_sequential(output_sizes)
+
+        if self.attention_type == 'multihead':
+            self.multihead_attention = MultiheadAttention(n_head)
+
+    def __call__(self, query, key, value):
+        if self.rep == 'mlp':
+            key = self.dk(key)
+            query = self.dq(query)
+
+        rep = self.attention_fn(query, key, value)
+        return rep
+
+    def attention_fn(self, query, key, value):
+        attentions = {
+            'uniform': uniform_attention,
+            'laplace': laplace_attention,
+            'dotprod': dotprod_attention
+        }
+        if self.attention_type != 'multihead':
+            attn = attentions[self.attention_type]
+        else:
+            attn = self.multihead_attention
+        return attn(query, key, value)
