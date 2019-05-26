de0a721e75179524673acf53556308a157c0724d
revsic <revsic99@gmail.com>
2019-05-27 03:04:06 +0900

    init: Add init files

---
+++ neural_process/__init__.py
@@ -0,0 +1,4 @@
+from neural_process.np import NeuralProcess
+from neural_process.cnp import ConditionalNP
+from neural_process.anp import AttentiveNP
+from neural_process.module.attention import Attention
