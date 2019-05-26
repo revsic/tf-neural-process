984777cd5cb41b20e56985b4b6fdeb1771f88b4e
revsic <revsic99@gmail.com>
2019-05-26 17:30:38 +0900

    attn: Fix attention shape err

--- attention.py
+++ attention.py
@@ -2,33 +2,47 @@ import tensorflow as tf
 
 import utils
 
-def uniform_attention(query, value):
-    total_points = tf.shape(query)[1]
+def uniform_attention(key, value):
+    # key : [B, T, X]
+    # value : [B, C, X]
+    total_points = tf.shape(key)[1]
     rep = tf.reduce_mean(value, axis=1, keepdims=True)
     rep = tf.tile(rep, [1, total_points, 1])
+    # [B, T, X]
     return rep
 
 
 def laplace_attention(query, key, value, scale, normalize):
-    key = tf.expand_dims(key, axis=1)
-    query = tf.expand_dims(query, axis=2)
+    # query : [B, C, X]
+    # key : [B, T, X]
+    # value : [B, C, X]
+    query = tf.expand_dims(query, axis=1)
+    key = tf.expand_dims(key, axis=2)
+    # unnorm_weights : [B, T, C, X]
     unnorm_weights = -tf.abs((key - query) / scale)
+    # unnorm_weights : [B, T, C]
     unnorm_weights = tf.reduce_sum(unnorm_weights, axis=-1)
     if normalize:
         weights = tf.nn.softmax(unnorm_weights)
     else:
         weights = 1 + tf.tanh(unnorm_weights)
+    # [B, T, X]
     return weights @ value
 
 
 def dot_prod_attention(query, key, value, normalize):
+    # query : [B, C, X]
+    # key : [B, T, X]
+    # value : [B, C, X]
     d_k = tf.shape(key)[-1]
     scale = tf.sqrt(tf.cast(d_k, tf.float32))
+    # unnorm_weights : [B, T, C]
     unnorm_weights = key @ tf.transpose(query, [0, 2, 1])
     if normalize:
         weights = tf.nn.softmax(unnorm_weights / scale)
     else:
         weights = tf.nn.sigmoid(unnorm_weights / scale)
+    # [B, T, X]
     return weights @ value
 
 
@@ -39,9 +53,8 @@ class MultiheadAttention:
     
     def __call__(self, query, key, value):
         if self.params is None:
-            d_k = key.shape[-1].value
             d_v = value.shape[-1].value
-            self.params = self.get_params(d_k, d_v)
+            self.params = self.get_params(d_v)
         
         rep = tf.constant(0.)
         for param in self.params:
@@ -53,7 +66,7 @@ class MultiheadAttention:
 
         return rep
 
-    def get_params(self, d_k, d_v):
+    def get_params(self, d_v):
         params = []
         head_size = d_v / self.n_head
         for _ in range(self.n_head):
@@ -87,11 +100,11 @@ class Attention:
 
     def __call__(self, key, query, value):
         if self.rep == 'mlp':
-            key = utils.batch_mlp(key, self.dk)
-            query = utils.batch_mlp(query, self.dq)
-        
+            key = self.dk(key)
+            query = self.dq(query)
+
         if self.attention_type == 'uniform':
-            rep = uniform_attention(query, value)
+            rep = uniform_attention(key, value)
         elif self.attention_type == 'laplace':
             rep = laplace_attention(query, key, value, self.scale, self.normalize)
         elif self.attention_type == 'dot_product':
