144dd4293c6bed628397a96113cd2bb824ee1179
revsic <revsic99@gmail.com>
2019-05-27 14:49:35 +0900

    tfutils: Add annotation

--- neural_process/module/tfutils.py
+++ neural_process/module/tfutils.py
@@ -2,6 +2,14 @@ import tensorflow as tf
 
 
 def dense_sequential(output_sizes, activation=tf.nn.relu):
+    """Convert number of hidden units to sequential dense layers
+    Args:
+        output_sizes: List[int], number of hidden units
+        activation: Callable[[tf.Tensor], tf.Tensor], activation function, default ReLU
+
+    Returns:
+        tf.keras.Model, sequential model consists of dense layers
+    """
     model = tf.keras.Sequential()
     for size in output_sizes[:-1]:
         model.add(tf.keras.layers.Dense(size, activation=activation))
